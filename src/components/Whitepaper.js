6c8bccefb87be8599d6e91e03ff571801a010948
kevinand11 <kevinfizu@gmail.com>
2023-07-22 04:04:01 +0100

    changes

--- src/components/Whitepaper.js
+++ src/components/Whitepaper.js
@@ -2,19 +2,20 @@ export default function Whitepaper (props) {
 	return <>
 		<p><b>TeleBots</b></p>
 
-<p><b>BrainyBot by $JEETS™</b> - Ask, Learn or Solve Anything… Right inside your telegram using this amazing Telebot with LOADS of Features!</p>
+		<p><b>BrainyBot by $JEETS™</b> - Ask, Learn or Solve Anything… Right inside your telegram using this amazing Telebot with LOADS of Features!</p>
 
-<p><b>Picasso by $JEETS™</b> - First ever AI Image Generator Telebot</p>
+		<p><b>Picasso by $JEETS™</b> - First ever AI Image Generator Telebot</p>
 
-Explore our useful and Fun Telebots! Steadily Updated and Convenient for us straight from your Telegram App! : <a href="https://linktr.ee/telebotsbyjeets" target="_blank">https://linktr.ee/telebotsbyjeets</a>
+		Explore our useful and Fun Telebots! Steadily Updated and Convenient for us straight from your Telegram App! : <a href="https://linktr.ee/telebotsbyjeets" target="_blank" rel="noreferrer">https://linktr.ee/telebotsbyjeets</a>
 
-<br />
-<br />
-<p><b>$JEETS lottery</b></p>
+		<br />
+		<br />
 
-<p>The $JEETS Contract is coded to Award Holders on a Lottery Basis! You stand a chance to win big by NOT being a F***ng Buzz kill and selling too early.</p>
+		<p><b>Tax Revenue Share</b></p>
 
-<p>And most importantly, We plan to Enjoy the Ride to the Moon!</p>
+		<p>The $JEETS Contract is coded to generate rewards to Holders from 1% of $JEETS traded volume. Prove yourself that you’re nothing like a Jeet and get rewarded for it!</p>
+
+		<p>Hold the set Minimum of $JEETS tokens and earn more $JEETS tokens every single second you hold! You will be able to claim your rewarded tokens at any time!</p>
 
 	</>
 }
