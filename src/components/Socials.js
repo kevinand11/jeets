e8356ad3f68973a8516b09ef90ee608b54af61dc
kevinand11 <kevinfizu@gmail.com>
2023-07-24 11:49:51 +0100

    gh-pages

--- src/components/Socials.js
+++
@@ -1,7 +0,0 @@
-export default function Socials (props) {
-	return <>
-		<p style={{ fontFamily: "Poly" }}>Twitter: <a href="https://twitter.com/jeets4l" target="_blank" rel="noreferrer">@jeets4l</a></p>
-
-		<p style={{ fontFamily: "Poly" }}>Telegram: <a href="https://t.me/jeets4l" target="_blank" rel="noreferrer">@jeets4l</a></p>
-	</>
-}
--- src/utils/index.js
+++
@@ -1,1 +0,0 @@
-export const ContractAddress = '0x7b47727B1624275bD90FB6856c7D542697460FC5'
